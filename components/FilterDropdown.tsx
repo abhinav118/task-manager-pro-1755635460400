9f92a0cb1b84c8bd6d716980316a609b9aa1b252
Abhi <abhinav118@gmail.com>
2025-08-19 16:31:10 -0400

    Add components/FilterDropdown.tsx

---
+++ components/FilterDropdown.tsx
@@ -0,0 +1,12 @@
+interface FilterDropdownProps {
+  trigger?: any
+  position?: any
+}
+
+export default function FilterDropdown({ trigger, position }: FilterDropdownProps) {
+  return (
+    <div className="bg-white shadow-lg border rounded-lg p-4 min-w-48">
+      {/* FilterDropdown component content */}
+    </div>
+  )
+}
